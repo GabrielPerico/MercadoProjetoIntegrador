7edf67f807b98fa8195e84446c5625076737a1b3
Gabriel <gabriel.perico@aluno.sc.senac.br>
2019-06-12 20:42:21 -0300

    Lista de imagens de cada produto

---
+++ application/views/ImagemProduto.php
@@ -0,0 +1,47 @@
+<?php
+$mensagem = $this->session->flashdata('mensagem');
+echo (isset($mensagem) ? '<div class="alert alert-success" role="alert">' . $mensagem . '</div>' : '');
+?>
+<div class="container my-5 pt-4">
+    <div class="row ">
+        <div class="col-md-12 mb-3">
+            <div class="card rounded-0 shadow-lg">
+                <div class="card-header bg-nav text-center text-white">
+                    <h3 class="mb-0">Imagens</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive shadow">
+                        <div class="text-center text-white">
+                            <a href="<?= $this->config->base_url() ?>Imagem/Cadastrar/<?= $id ?>" class="btn btn-outline-info">Cadastrar</a>
+                        </div>
+                        <table class="table semdatatable table-hover text-center mb-0">
+                            <thead class="bg-nav text-white">
+                                <tr>
+                                    <th scope="col">Imagem</th>
+                                    <th scope="col">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                if (count($imagens) > 0) {
+
+                                    foreach ($imagens as $i) {
+                                        echo '<tr>';
+                                        echo '<td class="align-middle table-cells" ><img class="img-fluid img-produto" src="' . $i->img_imagem . '"></td>';
+                                        echo '<td class="align-middle table-cells" >';
+                                        echo '<a data-toggle="tooltip" title="Deletar" class="btn btn-outline-danger mr-3" href="' . $this->config->base_url() . 'Produtos/Deletar/' . $i->ref_produto . '"><i class="fas fa-times-circle"></i></a>';
+                                        echo '</td>';
+                                        echo '</tr>';
+                                    }
+                                } else {
+                                    echo '<tr><td class"align-middle" width="100%" colspan="9">Não há produtos registrados</td></tr>';
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
